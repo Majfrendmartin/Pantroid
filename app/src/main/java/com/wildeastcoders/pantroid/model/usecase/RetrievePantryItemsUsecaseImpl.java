750fff3c56ec1fd0620a186182f2e2da8ed701b1
Majfrendmartin <raciborski.p@gmail.com>
2017-01-02 19:47:13 +0100

    Preparing RetrievePantryItemsUsecase tests and implementation.

---
+++ app/src/main/java/com/wildeastcoders/pantroid/model/usecase/RetrievePantryItemsUsecaseImpl.java
@@ -0,0 +1,17 @@
+package com.wildeastcoders.pantroid.model.usecase;
+
+import com.wildeastcoders.pantroid.model.PantryItem;
+
+import java.util.List;
+
+import rx.Observable;
+
+/**
+ * Created by Majfrendmartin on 2017-01-02.
+ */
+public class RetrievePantryItemsUsecaseImpl implements RetrievePantryItemsUsecase {
+    @Override
+    public Observable<List<PantryItem>> execute() {
+        return null;
+    }
+}
