c8082f4c2e1de5a5972cfdf78a553dcf5636c09d
Unknown <raciborski.p@gmail.com>
2017-04-08 17:18:39 +0200

    EditTypeFragmentTest part 1

--- app/src/test/java/com/wildeastcoders/pantroid/view/fragment/EditTypeFragmentTest.java
+++ app/src/test/java/com/wildeastcoders/pantroid/view/fragment/EditTypeFragmentTest.java
@@ -1,11 +1,11 @@
 package com.wildeastcoders.pantroid.view.fragment;
 
+import android.os.Bundle;
+
 import com.wildeastcoders.pantroid.BuildConfig;
 import com.wildeastcoders.pantroid.presenter.EditTypeFragmentPresenter;
 import com.wildeastcoders.pantroid.presenter.Presenter;
 import com.wildeastcoders.pantroid.utils.MockPantryItemTypesModule;
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +14,13 @@ import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import static com.wildeastcoders.pantroid.view.IntentConstants.KEY_EDIT_TYPE_ID;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+
 /**
  * Created by Majfrendmartin on 2017-03-26.
  */
@@ -21,6 +28,9 @@ import org.robolectric.annotation.Config;
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class EditTypeFragmentTest extends PresenterFragmentTest<EditTypeFragment> {
+
+    public static final long TYPE_ID = 1;
+    public static final String TEXT = "TEXT";
 
     @Mock
     private EditTypeFragmentPresenter presenter;
@@ -38,29 +48,33 @@ public class EditTypeFragmentTest extends PresenterFragmentTest<EditTypeFragment
         setup(fragment);
     }
 
-    @After
-    public void tearDown() throws Exception {
-
-    }
-
     @Test
     public void newInstance() throws Exception {
-
+        final EditTypeFragment fragment = EditTypeFragment.newInstance();
+        assertNotNull(fragment);
+        final Bundle arguments = fragment.getArguments();
+        assertNull(arguments);
     }
 
     @Test
     public void newInstance1() throws Exception {
-
-    }
-
-    @Test
-    public void onCreateView() throws Exception {
-
+        final EditTypeFragment fragment = EditTypeFragment.newInstance(TYPE_ID);
+        assertNotNull(fragment);
+        final Bundle arguments = fragment.getArguments();
+        assertNotNull(arguments);
+        assertTrue(arguments.containsKey(KEY_EDIT_TYPE_ID));
+        assertEquals(TYPE_ID, arguments.getLong(KEY_EDIT_TYPE_ID));
     }
 
     @Test
     public void onSaveClicked() throws Exception {
+        initializeFragment();
+        spyFragment.onSaveClicked();
+        verify(presenter).onSaveItemClicked(spyFragment.etTypeName.getText().toString());
 
+        spyFragment.etTypeName.setText(TEXT);
+        spyFragment.onSaveClicked();
+        verify(presenter).onSaveItemClicked(TEXT);
     }
 
     @Test
