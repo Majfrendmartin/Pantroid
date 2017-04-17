d0cb83aba76919e88767e1f1c15a9c31d501ee57
Unknown <raciborski.p@gmail.com>
2017-04-17 06:30:24 +0200

    Mocking EditItemFragmentPresenter

--- app/src/test/java/com/wildeastcoders/pantroid/utils/MockPantryItemsModule.java
+++ app/src/test/java/com/wildeastcoders/pantroid/utils/MockPantryItemsModule.java
@@ -3,12 +3,20 @@ package com.wildeastcoders.pantroid.utils;
 import android.support.annotation.NonNull;
 
 import com.wildeastcoders.pantroid.injection.module.PantryItemsModule;
+import com.wildeastcoders.pantroid.injection.scope.PerActivity;
+import com.wildeastcoders.pantroid.model.FieldsValidator;
+import com.wildeastcoders.pantroid.model.usecase.RetrievePantryItemTypesUsecase;
+import com.wildeastcoders.pantroid.model.usecase.RetrievePantryItemUsecase;
+import com.wildeastcoders.pantroid.model.usecase.SavePantryItemUsecase;
 import com.wildeastcoders.pantroid.presenter.EditItemActivityPresenter;
 import com.wildeastcoders.pantroid.presenter.EditItemFragmentPresenter;
+import com.wildeastcoders.pantroid.presenter.EditItemFragmentPresenterImpl;
 import com.wildeastcoders.pantroid.presenter.MainActivityPresenter;
 import com.wildeastcoders.pantroid.presenter.Presenter;
 
 import org.greenrobot.eventbus.EventBus;
+
+import dagger.Provides;
 
 /**
  * Created by Majfrendmartin on 2017-02-19.
@@ -39,5 +47,15 @@ public class MockPantryItemsModule extends PantryItemsModule {
     @Override
     public MainActivityPresenter provideMainActivityPresenter() {
         return (MainActivityPresenter) presenter;
+    }
+
+    @Override
+    public EditItemFragmentPresenter provideEditItemFragmentPresenter(
+            FieldsValidator fieldsValidator,
+            RetrievePantryItemUsecase retrievePantryItemUsecase,
+            RetrievePantryItemTypesUsecase retrievePantryItemTypesUsecase,
+            SavePantryItemUsecase savePantryItemUsecase,
+            EventBus eventBus) {
+        return (EditItemFragmentPresenter) presenter;
     }
 }
